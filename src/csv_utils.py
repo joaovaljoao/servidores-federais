75bd027c4d4b25e1e04616b7251500a9a58579b9
João Carlos <joao.oliveira@ufob.edu.br>
2023-01-31 11:33:09 -0300

    Refatorando para economizar memória

--- src/csv_utils.py
+++ src/csv_utils.py
@@ -2,13 +2,27 @@ import os
 import pandas as pd
 import gc
 
+
 def filter_csv(file):
-    df = pd.read_csv(file, encoding='latin-1', low_memory=False, sep=';')
-    df = df[(df['COD_ORG_LOTACAO'] == 26447) | (df['COD_ORG_EXERCICIO'] == 26447) | (df['COD_UORG_LOTACAO'] == 26232000000732)]
+    chunk_size = 10**6 # adjust chunk size to balance memory usage and processing time
+    df_filtered = pd.DataFrame()
+    dtype = {'COD_ORG_LOTACAO': 'int32', 'COD_ORG_EXERCICIO': 'int32', 'COD_UORG_LOTACAO': 'int64'}
+    for chunk in pd.read_csv(file, chunksize=chunk_size, encoding='latin-1', low_memory=False, sep=';', dtype=dtype):
+        df_filtered = pd.concat([df_filtered, chunk[(chunk['COD_ORG_LOTACAO'] == 26447) | (chunk['COD_ORG_EXERCICIO'] == 26447) | (chunk['COD_UORG_LOTACAO'] == 26232000000732)]])
     new_file = file.replace('.csv', '_ufob.csv')
-    df.to_csv(new_file, index=False)
+    df_filtered.to_csv(new_file, index=False)
     os.remove(file)
-    return  new_file.split('/')[-1]
+    return new_file.split('/')[-1]
+
+
+
+# def filter_csv(file):
+#     df = pd.read_csv(file, encoding='latin-1', low_memory=False, sep=';')
+#     df = df[(df['COD_ORG_LOTACAO'] == 26447) | (df['COD_ORG_EXERCICIO'] == 26447) | (df['COD_UORG_LOTACAO'] == 26232000000732)]
+#     new_file = file.replace('.csv', '_ufob.csv')
+#     df.to_csv(new_file, index=False)
+#     os.remove(file)
+#     return  new_file.split('/')[-1]
 
 def read_and_clean_data(filepath):
     df = pd.read_csv(filepath)
