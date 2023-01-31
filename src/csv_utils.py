32439935ebe8e31007881fedc2ad77b820c0ab1c
João Carlos <joao.oliveira@ufob.edu.br>
2023-01-31 12:32:43 -0300

    Refatorando para economizar memória

--- src/csv_utils.py
+++ src/csv_utils.py
@@ -1,14 +1,33 @@
 import os
 import pandas as pd
 import gc
+import csv
 
 
-def filter_csv(file):
-    df = pd.read_csv(file, encoding='latin-1', low_memory=False, sep=';')
-    df = df.query("COD_ORG_LOTACAO == 26447 or COD_ORG_EXERCICIO == 26447 or COD_UORG_LOTACAO == 26232000000732")
+def filter_csv(file, chunk_size=1000):
+    filtered_data = []
+    with open(file, 'r', encoding='latin1') as f:
+        reader = csv.reader(f, delimiter=';')
+        header = next(reader)
+        filtered_data.append(header)
+        chunk = []
+        unique_ids = set()
+        for i, row in enumerate(reader):
+            if row[17] == '26447' or row[23] == '26447' or row[15] == '26232000000732':
+                id_servidor_portal = row[0]
+                if id_servidor_portal not in unique_ids:
+                    chunk.append(row)
+                    unique_ids.add(id_servidor_portal)
+            if (i + 1) % chunk_size == 0:
+                filtered_data.extend(chunk)
+                chunk = []
+        filtered_data.extend(chunk)
+    
     new_file = file.replace('.csv', '_ufob.csv')
-    df.to_csv(new_file, index=False)
-    os.remove(file)
+    with open(new_file, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerows(filtered_data)
+    
     return new_file.split('/')[-1]
 
 
@@ -22,7 +41,7 @@ def filter_csv(file):
 #     return  new_file.split('/')[-1]
 
 def read_and_clean_data(filepath):
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, encoding='latin-1', sep=';')
     df = df[['Id_SERVIDOR_PORTAL', 'NOME', 'CPF', 'MATRICULA']]
     df['CNPQ_ID'] = None
     df['CNPQ_WEB_ID'] = None
@@ -32,13 +51,14 @@ def read_and_clean_data(filepath):
 
 def concatenate_csv_files(folder_path, output_file):
     csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_file, 'w', encoding='latin1') as f:
         first_file = True
         for csv_file in csv_files:
-            df = pd.read_csv(os.path.join(folder_path, csv_file))
-            if first_file:
-                df.to_csv(f, index=False, header=True)
-                first_file = False
-            else:
-                df.to_csv(f, index=False, header=False, mode='a')
+            if csv_file.endswith('_ufob.csv'):
+                df = pd.read_csv(os.path.join(folder_path, csv_file), encoding='latin1', sep=';')
+                if first_file:
+                    df.to_csv(f, index=False, header=True, encoding='latin1', sep=';')
+                    first_file = False
+                else:
+                    df.to_csv(f, index=False, header=False, mode='a', encoding='latin1', sep=';' )
     return 
