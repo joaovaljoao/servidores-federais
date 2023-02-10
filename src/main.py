5b5b0c219f5f8ced93e47fe147563a754656710c
João Carlos <joao.oliveira@ufob.edu.br>
2023-02-09 22:01:53 -0300

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,32 +1,30 @@
-from s3 import upload_to_s3
-from csv_utils import filter_csv, read_and_clean_data, concatenate_csv_files
-from rds_utils import create_table_in_rds
+import s3
+import csv
+from rds import Servidores
 from download import download_servidores
 import os
 import pandas as pd
 
 def main():
     bucket_name = 'servidores-ufob'    
-    folder = 'output/'
+    folder = 'stage/'
 
-    for ano in range(2013, 2023):
-        for mes in range(1, 13):
-            # download the file
-            download_servidores(ano, mes, folder)
-            file = f'{ano}{mes:02d}_Cadastro.csv'
-            if file.endswith('_Cadastro.csv'):
-                filtered_file = filter_csv(folder + file)
-                upload_to_s3(folder, filtered_file, bucket_name)
-                read_and_clean_data(folder + filtered_file)
-    
-    concatenate_csv_files(folder, 'servidores_ufob.csv')
-        # drop duplicates
-    df = pd.read_csv('servidores_ufob.csv')
-    print('csv importado')
-    df.drop_duplicates(subset=['Id_SERVIDOR_PORTAL'], inplace=True)
-    print('duplicatas removidas')
-    create_table_in_rds(df)
+    # `read dfs from folder and create table called cargos
+    files = os.listdir(folder)
+    # create cargos.csv
+    df_list = []
+    for file in files:
+        df = pd.read_csv(os.path.join(folder, file), encoding='utf-8', sep=';')
+        df = df[df['TIPO_VINCULO'] == 'Cargo']
+        df['ano'] = file[:4]
+        df['mes'] = file[4:6]
+        df = df[["Id_SERVIDOR_PORTAL", "DESCRICAO_CARGO", "CLASSE_CARGO", "REFERENCIA_CARGO", "PADRAO_CARGO", "NIVEL_CARGO", "COD_UORG_LOTACAO", "UORG_LOTACAO", "COD_ORG_LOTACAO", "ORG_LOTACAO", "COD_ORGSUP_LOTACAO", "ORGSUP_LOTACAO", "COD_UORG_EXERCICIO", "UORG_EXERCICIO", "COD_ORG_EXERCICIO", "ORG_EXERCICIO", "COD_ORGSUP_EXERCICIO", "ORGSUP_EXERCICIO", "TIPO_VINCULO", "SITUACAO_VINCULO", "REGIME_JURIDICO", "JORNADA_DE_TRABALHO", "DATA_INGRESSO_CARGOFUNCAO", "DATA_NOMEACAO_CARGOFUNCAO", "DATA_INGRESSO_ORGAO", "DOCUMENTO_INGRESSO_SERVICOPUBLICO", "DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO", "DIPLOMA_INGRESSO_ORGAO", "DIPLOMA_INGRESSO_SERVICOPUBLICO", "UF_EXERCICIO", "ano", "mes"]]
+        df_list.append(df)
+    df = pd.concat(df_list, axis=0, ignore_index=True)
+    Servidores().create_table(df, 'cargos')
+
 
 
 if __name__ == '__main__':
-    main() 
+    df = pd.read_excel('Pasta1.xlsx')
+    Servidores().create_table(df, 'servidores')
--- src/s3.py
+++ src/s3.py
@@ -6,3 +6,20 @@ def upload_to_s3(folder, filename, bucket_name):
     file_path = os.path.join(folder, filename)
     s3.upload_file(file_path, bucket_name, filename)
     print(f'Successfully uploaded {filename} to {bucket_name}.')
+
+def download_from_s3(bucket_name, folder):
+    s3 = boto3.client('s3')
+    files = []
+    for obj in s3.list_objects(Bucket=bucket_name)['Contents']:
+        filename = obj['Key']
+        if filename.endswith('.csv'):
+            files.append(filename)
+            s3.download_file(bucket_name, filename, os.path.join(folder, filename))
+            print(f'Successfully downloaded {filename} from {bucket_name}.')
+    return files\
+
+def download_file_from_s3(bucket_name, folder, filename):
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, filename, os.path.join(folder, filename))
+    print(f'Successfully downloaded {filename} from {bucket_name}.')
+    
